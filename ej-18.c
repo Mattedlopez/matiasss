544347d0865f20683b2a60af70419fd58d47369a
Mattedlopez <130605448+Mattedlopez@users.noreply.github.com>
2023-04-18 12:59:17 -0500

    Update ej-18.c

--- ej-18.c
+++ ej-18.c
@@ -2,20 +2,21 @@
 #include <math.h>
 
 int main (){
-    int top, bttn, dif, n=0, i=0; 
+    int top, bttn, dif, n=0, i=0; //declaracion de variables
+    //ingreso de variables
     printf("ingresa numero base: ");
     scanf("%d", &bttn);
     printf("ingresa numero tope: ");
     scanf("%d", &top);
-
+    //definicion de la varibale dif
     dif = top-bttn;
 
-    if ( dif > 5){
-        while (n < dif){
-            n += 5;
-            i++;
+    if ( dif > 5){//condicion
+        while (n < dif){//lazo while
+            n += 5;//incremento del numero
+            i++;//incremento del contador
         }
-        printf("hay %d multiplos de 5", i); 
+        printf("hay %d multiplos de 5", i); //resultado
     }else{
         printf("No hay multiplos de 5 entre los numeros dados");
     }
