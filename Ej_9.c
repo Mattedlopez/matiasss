4c07a4f9359c76cc953bbf340fb930198500fe08
Mattedlopez <130605448+Mattedlopez@users.noreply.github.com>
2023-04-18 12:57:17 -0500

    Update Ej_9.c

--- Ej_9.c
+++ Ej_9.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main (){
-    int num=26;
-    while (num<205){
-        printf("%d\n", num);
-        num=num+10;
+    int num=26;//declaracion de variables
+    while (num<205){//lazo while con condicion
+        printf("%d\n", num);//impresion de resultado
+        num=num+10;//inremento
     }
     
     printf("%d", num);
