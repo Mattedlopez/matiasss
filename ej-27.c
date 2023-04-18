e5964ba271b1b18ff6309c96d650a5d0e71b7ac4
Mattedlopez <130605448+Mattedlopez@users.noreply.github.com>
2023-04-18 13:00:54 -0500

    Update ej-27.c

--- ej-27.c
+++ ej-27.c
@@ -2,21 +2,21 @@
 #include <math.h>
 
 int main (){
-    int x,y,nx=0, ny=0;
-    printf("ingrese numero x: ");
+    int x,y,nx=0, ny=0;//declaracion de variables
+    printf("ingrese numero x: ");//ingreso de variables
     scanf("%d", &x);
     printf("ingrese numero y: ");
     scanf("%d", &y);
 
-    while (x > 1){
-        x = x/10;
-        nx++;
+    while (x > 1){//lazo while y condicion
+        x = x/10;//divicion por 10
+        nx++;//incremento de contador
     }    
-    while (y > 1){
+    while (y > 1){//mismo paso para la otra variable
         y = y/10;
         ny++;
     }
-    if (nx > ny){
+    if (nx > ny){//comparacion de contadores
         printf("El numero x tiene mas digitos");
     }else{
         printf("El numero y tiene mas digitos");
